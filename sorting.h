bae61291257855265b3cbd0f1022ca4f63374e82
Veronica Ocampo <veronicao2004@gmail.com>
2025-04-15 11:47:29 -0400

    Update sorting.h + quickSort
    added quick sort stuff

--- sorting.h
+++ sorting.h
@@ -2,7 +2,10 @@
 #include <vector>
 using namespace std;
 
+void merge(vector<int>& array, int left, int mid, int right);
 void mergeSort(vector<int>& array, int left, int right);
-//quick sort
+
+int splitArray(vector<int>& array, int startIndex, int endIndex);
+void quickSort(vector<int>& array, int startIndex, int endIndex);
 //timer stuff
 //blah blah blah etc etc etc
